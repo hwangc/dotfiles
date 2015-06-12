1052c49e730ec9771e313e43e6c579928eb37301
HwangC <hwangc@hwangc.com>
2015-06-12 19:22:16 +0900

    fix: path problem

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,15 +6,15 @@
 
 ########## Variables
 
-dir='~/dotfiles'                    # dotfiles directory
-olddir='~/dotfiles_old'             # old dotfiles backup directory
-files="vimrc zshrc tmux.conf zpreztorc"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+declare -a files=("vimrc" "zshrc" "tmux.conf" "zpreztorc")    # list of files/folders to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 echo "done"
 
 # change to the dotfiles directory
@@ -23,9 +23,10 @@ cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
+for file in "${files[@]}"
+do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/".$file" ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s "$dir"/"$file" ~/".$file"
 done
