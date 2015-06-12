5a2d9aa11c03124dc29804060745687511ec194d
HwangC <hwangc@hwangc.com>
2015-06-12 18:56:24 +0900

    fix: directory with space

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir='~/dotfiles'                    # dotfiles directory
+olddir='~/dotfiles_old'             # old dotfiles backup directory
 files="vimrc zshrc tmux.conf zpreztorc"    # list of files/folders to symlink in homedir
 
 ##########
